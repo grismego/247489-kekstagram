1b7706cb5f068837388a4c994e1a332f2156fc8a
Алексей Кунчий <gris.mego@gmail.com>
2018-12-14 14:27:18 +0700

    filechooser

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@
 
 (function () {
 
+  // var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
   var uploadFormElement = document.querySelector('#upload-select-image');
   var uploadElement = document.querySelector('.img-upload');
   var uploadPopupElement = uploadElement.querySelector('.img-upload__overlay');
@@ -16,6 +18,9 @@
   var successModalTemplate = document.querySelector('#success').content.querySelector('.success');
   var mainElement = document.querySelector('main');
 
+  // var imgPreviewWrapperElement = uploadElement.querySelector('.img-upload__preview');
+  // var imgPreviewElement = imgPreviewWrapperElement.querySelector('.img-upload__preview img');
+
 
   var openForm = function () {
     uploadPopupElement.classList.remove('hidden');
@@ -28,6 +33,8 @@
   var closeForm = function () {
     uploadPopupElement.classList.add('hidden');
     uploadFileElement.value = null;
+    descriptionElement.value = null;
+    hashtagElement.value = null;
     document.removeEventListener('keydown', onFormEscPress);
   };
 
@@ -37,6 +44,22 @@
 
   uploadFileElement.addEventListener('change', function () {
     openForm();
+
+    // var file = uploadFileElement.files[0];
+    // var filename = file.name.toLowerCase();
+    //
+    // var matches = FILE_TYPES.some(function (it) {
+    //   return filename.endsWith(it);
+    // });
+    // if (matches) {
+    //   var reader = new FileReader();
+    //
+    //   reader.addEventListener('load', function () {
+    //     imgPreviewElement.src = reader.result;
+    //   });
+    //
+    //   reader.readAsDataURL(file);
+    // }
   });
 
   uploadPopupCloseElement.addEventListener('click', function () {
